9d3130bdf97a5ca38aeb60091c45e3fa39126f26
심태진 <simtaejin@simtaejin-ui-MacBookPro.local>
2022-07-10 22:49:02 +0900

    first react component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
-import logo from './logo.svg';
+import React, { Component } from "react";
 import './App.css';
+import Dashboard from "./components/Dashboard";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1 className="App-title">Welcom to PPMTool</h1>
-      </header>
-
-    </div>
-  );
+class App extends Component {
+  render() {
+      return (
+          <div className="App">
+              <Dashboard />
+          </div>
+      );
+  }
 }
 
 export default App;
