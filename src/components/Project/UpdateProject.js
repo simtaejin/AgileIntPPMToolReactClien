0122e8283d0f3a860ac1fe9e76c7ea590ea118aa
심태진 <simtaejin@simtaejin-ui-MacBookPro.local>
2022-07-16 05:26:53 +0900

    Handle Errors in UpdateProject.js

--- src/components/Project/UpdateProject.js
+++ src/components/Project/UpdateProject.js
@@ -15,13 +15,18 @@ class UpdateProject extends Component {
             projectIdentifier: "",
             description: "",
             start_date: "",
-            end_date: ""
+            end_date: "",
+            errors: {}
         };
         this.onChange = this.onChange.bind(this);
         this.onSubmit = this.onSubmit.bind(this);
     }
 
     componentWillReceiveProps(nextProps) {
+        if (nextProps.errors) {
+            this.setState({ errors: nextProps.errors });
+        }
+
         const {
             id,
             projectName,
@@ -66,6 +71,8 @@ class UpdateProject extends Component {
     }
 
     render() {
+        const { errors } = this.state;
+
         return (
             <div>
                 <div className="project">
@@ -76,12 +83,17 @@ class UpdateProject extends Component {
                             <hr/>
                             <form onSubmit={this.onSubmit}>
                                 <div className="form-group">
-                                    <input type="text" className="form-control form-control-lg "
+                                    <input type="text" className={classnames("form-control form-control-lg", {
+                                        "is-invalid": errors.projectName
+                                    })}
                                            placeholder="Project Name"
                                            name="projectName"
                                            value={this.state.projectName}
                                            onChange={this.onChange}
                                     />
+                                    {errors.projectName && (
+                                        <div className="invalid-feedback">{errors.projectName}</div>
+                                    )}
                                 </div>
                                 <div className="form-group">
                                     <input type="text" className="form-control form-control-lg"
@@ -123,11 +135,13 @@ class UpdateProject extends Component {
 UpdateProject.prototypes = {
     getProject: PropTypes.func.isRequired,
     createProject: PropTypes.func.isRequired,
-    project: PropTypes.object.isRequired
+    project: PropTypes.object.isRequired,
+    errors: PropTypes.object.isRequired
 }
 
 const mapStateToProps = state => ({
-    project: state.project.project
+    project: state.project.project,
+    errors: state.errors
 });
 
 export default connect(
