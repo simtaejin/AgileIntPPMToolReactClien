f38ab2b0e800fb5220494bf761807642ee53e083
심태진 <simtaejin@simtaejin-ui-MacBookPro.local>
2022-07-24 10:34:52 +0900

    Delete Project Task

--- src/actions/backlogActions.js
+++ src/actions/backlogActions.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { GET_ERRORS, GET_BACKLOG, GET_PROJECT_TASK} from "./types";
+import {GET_ERRORS, GET_BACKLOG, GET_PROJECT_TASK, DELETE_PROJECT_TASK} from "./types";
 
 export const addProjectTask = (
     backlog_id,
@@ -61,5 +61,15 @@ export const updateProjectTask = (backlog_id, pt_id, project_task, history) => a
             type: GET_PROJECT_TASK,
             payload: err.response.data
         })
+    }
+}
+
+export const deleteProjectTask = (backlog_id, pt_id) => async dispatch => {
+    if (window.confirm(`You are deleteing project task ${pt_id}, this action cannot be undone`)) {
+        await axios.delete(`/api/backlog/${backlog_id}/${pt_id}`)
+        dispatch({
+            type:DELETE_PROJECT_TASK,
+            payload: pt_id
+        });
     }
 }
