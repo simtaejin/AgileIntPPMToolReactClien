5229403c23044db9a04e343ce15e73ca508773e7
심태진 <simtaejin@simtaejin-ui-MacBookPro.local>
2022-07-31 23:17:06 +0900

    Lock public routes when logged in

--- src/components/UserManagement/Register.js
+++ src/components/UserManagement/Register.js
@@ -19,6 +19,12 @@ class Register extends Component {
         this.onSubmit = this.onSubmit.bind(this);
     }
 
+    componentDidMount() {
+        if (this.props.security.validToken) {
+            this.props.history.push("/dashboard")
+        }
+    }
+
     componentWillReceiveProps(nextProps) {
         if (nextProps.errors) {
             this.setState({ errors: nextProps.errors });
@@ -126,11 +132,13 @@ class Register extends Component {
 
 Register.propTypes = {
     createNewUser: PropTypes.func.isRequired,
-    errors: PropTypes.object.isRequired
+    errors: PropTypes.object.isRequired,
+    security: PropTypes.object.isRequired
 };
 
 const mapStateToProps = state => ({
-    errors: state.errors
+    errors: state.errors,
+    security: state.security
 });
 
 export default connect(
